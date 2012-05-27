30d22ea98ddcc39b949fe556df40edc5f07c4949
Rahul Rajagopalan <rahulrajago@gmail.com>
2012-05-27 16:40:52 -0400

    Formatting

--- lib/spec/class_mirrorer_spec.rb
+++ lib/spec/class_mirrorer_spec.rb
@@ -6,13 +6,13 @@ describe 'ClassMirrorer' do
   end
 
   it 'mirrors given classes' do
-    @stub_parser.stub(:translate).and_return('translated') 
+    @stub_parser.stub(:translate).and_return :translated
     mirrorer = VirtualKeywords::ClassMirrorer.new @stub_parser
     result = mirrorer.mirror [Fizzbuzzer]
     
     class_and_method = VirtualKeywords::ClassAndMethodName.new(
         Fizzbuzzer, 'fizzbuzz')
     result.keys.should include class_and_method
-    result[class_and_method].should eql 'translated'
+    result[class_and_method].should eql :translated
   end
 end
